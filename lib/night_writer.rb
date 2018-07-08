c38cc8ff624164e45944f5c5a7f67a0acfb670b4
dmeskis <dmeskis@gmail.com>
2018-07-08 14:26:49 -0600

    program works up to step 4 of 2nd iteration

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -13,15 +13,30 @@ argv_2 = ARGV[1]
 
 # Opening our message file into a variable
 message_file = File.open(ARGV[0]) {|f| f.read() }
+
+message_translated = []
+message_file.chars.each do |char|
+  message_translated << dictionary.braille_dictionary[char]
+  # require 'pry' ; binding.pry
+end
 # Write code to convert contents of our message_file to braille using our
 # dictionary class and methods. Then write this into our braille file.
+# converted_to_braille = message_file.chars.each do |char|
+#                           if char == dictionary.braille_dictionary.keys
+#                             char = dictionary.braille_dictionary.value
+#                             require 'pry' ; binding.pry
+#                           else
+#                           end
+#                         end
 
-braille_file = File.open(ARGV[1], "w") {|f| f.write(message_file) }
+# converted_content message_file
+
+braille_file = File.open(ARGV[1], "w") {|f| f.write(message_translated.join) }
 
 
 
 # Assign a variable the number of characters in arg_2 and
 # interpolate this value below
-arg_1_char_length = message_file.size
-arg_2_char_length = braille_file.size
-puts "Created #{arg_2} containing #{arg_1_char_length} characters"
+argv_1_char_length = message_file.size - 1
+argv_2_char_length = braille_file.size
+puts "Created #{argv_2} containing #{argv_1_char_length} characters"
