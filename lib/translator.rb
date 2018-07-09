1b02a3e375cfdbc31dda8c81c42847ed6bd04e5a
dmeskis <dmeskis@gmail.com>
2018-07-09 12:10:42 -0600

    Created three new methods in translator class

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,21 +1,33 @@
 require_relative 'braille_dictionary'
 
 class Translator
-  attr_reader :argv_1, :argv_2, :translated_message
+  attr_reader :input, :output, :translated_message, :dictionary
 
-  def initialize(argv_1, argv_2)
-    @argv_1 = argv_1
-    @argv_2 = argv_2
+  def initialize(input, output)
+    @input = input
+    @output = output
     @translated_message = []
+    @dictionary = BrailleDictionary.new()
   end
 
-  def open_file(file)
-    File.open(file)
+  def open_file_and_read_file(file)
+    File.open(file) {|f| f.read() }
   end
 
-  def read_file
-
+  def translate_file
+    message = open_file_and_read_file(input)
+    message.chars.each do |char|
+      @translated_message << @dictionary.braille_dictionary[char]
+    end
+    @translated_message
   end
+
+  def write_to_output
+    File.open(output, "w") {|f| f.write(@translated_message.join)}
+  end
+
+  # Create three methods that collect three different arrays based on which line
+  # 
 
 
 end
