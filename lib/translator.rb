6cfad2654d0fbc56d1ebe01025de2be1d255602f
dmeskis <dmeskis@gmail.com>
2018-07-09 15:25:24 -0600

    Fixed the nil problem

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ class Translator
   end
 
   def translate_file
-    message = open_input_file_and_read_file
+    message = open_input_file_and_read_file.chomp
     message.chars.each do |char|
       @translated_message << @dictionary.braille_dictionary[char]
     end
@@ -26,16 +26,20 @@ class Translator
     line_1 = collect_first_line_array
     line_2 = collect_second_line_array
     line_3 = collect_third_line_array
-    line_1_excess_array = []
-    line_2_excess_array = []
-    line_3_excess_array = []
     f = File.open(output, "w")
-    line_1[0].each {|line| f.print(line)}
+    line_1.each {|line| f.print(line)}
     f.print("\n")
-    line_2[0].each {|line| f.print(line)}
+    line_2.each {|line| f.print(line)}
     f.print("\n")
-    line_3[0].each {|line| f.print(line)}
+    line_3.each {|line| f.print(line)}
     f.close
+  end
+
+  def split_lines_over_80_char(line)
+    excess_array = []
+    if line.count > 40
+      excess_array << collect_first_line_array[40..-1]
+    end
   end
 
   # Create three methods that collect three different arrays based on which line
@@ -43,28 +47,28 @@ class Translator
   def collect_first_line_array
     first_line_array = []
     first_line_array << @translated_message.map do |element|
-      element[0] unless element == nil
+      element[0]
     end
-    first_line_array[0].pop
-    first_line_array
+    first_line_array[0]
+  #   first_line_array[0]
   end
 
   def collect_second_line_array
     second_line_array = []
     second_line_array << @translated_message.map do |element|
-      element[1] unless element == nil
+      element[1]
     end
-    second_line_array[0].pop
-    second_line_array
+    # second_line_array[0].pop
+    second_line_array[0]
   end
 
   def collect_third_line_array
     third_line_array = []
     third_line_array << @translated_message.map do |element|
-      element[2] unless element == nil
+      element[2]
     end
-    third_line_array[0].pop
-    third_line_array
+    # third_line_array[0].pop
+    third_line_array[0]
   end
 
 
