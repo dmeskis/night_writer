39350acfd7f1ee2f26e1ac226d86908f7fe2fb1d
dmeskis <dmeskis@gmail.com>
2018-07-09 09:25:47 -0600

    refactoring in translator.rb

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'braille_dictionary'
 dictionary = BrailleDictionary.new()
 
@@ -19,17 +20,8 @@ message_file.chars.each do |char|
   message_translated << dictionary.braille_dictionary[char]
   # require 'pry' ; binding.pry
 end
-# Write code to convert contents of our message_file to braille using our
-# dictionary class and methods. Then write this into our braille file.
-# converted_to_braille = message_file.chars.each do |char|
-#                           if char == dictionary.braille_dictionary.keys
-#                             char = dictionary.braille_dictionary.value
-#                             require 'pry' ; binding.pry
-#                           else
-#                           end
-#                         end
 
-# converted_content message_file
+# message_translated_as_string = message_translated.join
 
 braille_file = File.open(ARGV[1], "w") {|f| f.write(message_translated.join) }
 
--- lib/translator.rb
+++ lib/translator.rb
@@ -1,3 +1,10 @@
 class Translator
+  attr_reader :argv_1, :argv_2
+
+  def initialize(argv_1, argv_2)
+    @argv_1 = argv_1
+    @argv_2 = argv_2
+  end
+
 
 end
