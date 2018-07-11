bddef22b8cf5e7ee0cdf7aa92c308f09a4cfa820
dmeskis <dmeskis@gmail.com>
2018-07-10 18:31:48 -0600

    Added several methods

--- lib/reverse_translator.rb
+++ lib/reverse_translator.rb
@@ -17,12 +17,23 @@ class ReverseTranslator
   # arrays creates a key in the dictionary corresponding to a letter
   def get_braille_arrays
     string = braille_to_string
-    lines = string.scan(/.{80}/)
+    lines = string.scan(/.{1,80}/)
     pairs = lines.map do |string|
       string.scan(/../)
     end
   end
 
+  # Scan the arrays you got into groups of 3 to prep for zip method
+  def group_braille_arrays
+    get_braille_arrays.each_slice(3).to_a
+  end
+
+  def zip_grouped_braille_arrays
+    zipped_arrays = group_braille_arrays
+    zipped_arrays.map do |array|
+      array[0].zip(array[1], array[2])
+    end
+  end
 
   def collect_line_array(line_number)
 
